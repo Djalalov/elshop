2732b72abab89813b41a465157df3d03a4a93c03
Djalalov <edjalalov@outlook.com>
2022-01-10 13:39:06 +0500

    setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
