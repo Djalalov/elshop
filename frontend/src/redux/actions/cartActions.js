92584e2bd6086a1ba55a660fc1aea774eb6cb98f
Djalalov <edjalalov@outlook.com>
2022-06-12 02:58:59 +0500

    updates

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
 	const { data } = await axios.get(`/api/products/${id}`);
+
 	dispatch({
 		type: actionTypes.ADD_TO_CART,
 		payload: {
--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -6,7 +6,7 @@ export const getProducts = () => async dispatch => {
 		dispatch({ type: actionTypes.GET_PRODUCTS_REQUEST });
 
 		const { data } = await axios.get("/api/products");
-
+		console.log(data);
 		dispatch({
 			type: actionTypes.GET_PRODUCTS_SUCCESS,
 			payload: data,
